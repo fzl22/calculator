257b8ebcffecbb620f5e4fe9f84a04d59458fb93
csit22 <neha.linjhara22@gmail.com>
2023-04-22 18:34:28 +0530

    changed

--- Calculator.java
+++ Calculator.java
@@ -17,14 +17,11 @@ public int subtract(int a, int b){
 return a-b;
 }
 
-<<<<<<< HEAD
 public double subtract(double a, double b){
 return a-b;
 }
 
-=======
 public int multiply(int a, int b){
 return a * b;
 }
->>>>>>> featureA
 }
